8e52110ed994e40931f15b473c71e1f7a9ba28df
benfindley18 <ben.kenzieacademy@gmail.com>
2020-07-22 15:59:38 -0400

    created react app

---
+++ src/Components/Nav-bar/Nav.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './Nav.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+
+class Navigation extends React.Component {
+    state = {}
+
+
+    render() {
+
+        return (
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="navbar">
+                    <Nav className="dropdown">
+                        <Nav.Link href="#features"> Home </Nav.Link>
+                        <Nav.Link href="#pricing"> Contact </Nav.Link>
+                        <NavDropdown title=" Projects " id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1"> Hive </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2"> Password Strength </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3"> Plume </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
+}
+
+
+export default Navigation;
